eded742c91467961563b7b71ea8f4c58b6d713e0
ArgentumHeart <vadym.salnyk@gmail.com>
2023-10-17 14:35:50 +0300

    fix #2. missing overflowY property on classes list

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -32,7 +32,7 @@ export default function App() {
               position: 'relative',
             }}
           >
-            <Box>
+            <Box sx={{ overflowY: 'auto' }}>
               <Box
                 sx={{
                   padding: '20px',
