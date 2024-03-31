d538b4293fc2e09d70d1f21b483921ae5dde3cdf
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2024-03-31 10:48:31 +0200

    Added missing hyrule prophet icons, fixed crafting recipes

--- src/icons/hyruleProphetIcons.ts
+++ src/icons/hyruleProphetIcons.ts
@@ -1,9 +1,17 @@
 import AngelicHeart from './png/evo/hyruleProphet/AngelicHeart.png';
 import AngelicHope from './png/evo/hyruleProphet/AngelicHope.png';
 import HarbingersHeart from './png/evo/hyruleProphet/HarbingersHeart.png';
+import DisenchantTriforceRelics from './png/evo/hyruleProphet/BTNDH_Holy_Shields.png'
+import PureLightSynthesis from './png/evo/hyruleProphet/BTNLightStrike.png'
+import IgnitedIncinerator from './png/evo/hyruleProphet/IgnitedIncinerator.png'
+import PureIgnitedIncinerator from './png/evo/hyruleProphet/PureIgnitedIncinerator.png'
 
 export const hyruleProphetIcons = {
   AngelicHeart,
   AngelicHope,
   HarbingersHeart,
+  DisenchantTriforceRelics,
+  PureLightSynthesis,
+  IgnitedIncinerator,
+  PureIgnitedIncinerator,
 }
